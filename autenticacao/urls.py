1205618aa8ed9f6582d3c7c9549ae13cd17aab66
paulaandrezza <paulaandrezza25@gmail.com>
2023-11-04 13:14:35 -0300

    feat: remove index

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.auth),
     path('cadastro/', views.cadastro),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,8 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def auth(request):
-  return HttpResponse("Você está na autenticação")
-
 def cadastro(request):
   return HttpResponse('Faça seu cadastro')
